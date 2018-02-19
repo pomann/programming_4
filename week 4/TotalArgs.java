5beafc5ae7a9c4f0f09aaf1207c6143ea5f44304
Roman Prochazka <36170515+pomann@users.noreply.github.com>
2018-02-19 16:41:04 +0000

    Add files via upload

---
+++ week 4/TotalArgs.java
@@ -0,0 +1,11 @@
+public class TotalArgs
+{
+    public static void main(String [] args)
+    {
+        int sum = 0;
+        for(int i = 0; i < args.length; i++){
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println("The sum of all the args is "+sum+".");
+    }
+}
