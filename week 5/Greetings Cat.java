4f3ac0d96116f757314055455fb7cb7c537edcc3
Roman Prochazka <36170515+pomann@users.noreply.github.com>
2018-02-26 21:14:16 +0000

    Add files via upload

---
+++ week 5/Greetings Cat.java
@@ -0,0 +1,19 @@
+// This class cant do much. It can be created and printed.
+public class Cat implements Polite
+{
+    private String name;
+    
+    public Cat(String n)
+    {
+        name = n;
+    }
+    
+    public String toString()
+    {
+        return "cat: " + name;
+    }
+    
+    public void hello(){
+        System.out.println("Greetings");
+    }
+}
